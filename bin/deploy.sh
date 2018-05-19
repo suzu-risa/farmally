bab1fdbee551982fb06132f3a34fa08ba94401f8
tomoya <tom@tomhonjo.me>
2018-05-19 17:13:41 +0900

    Fix deploy env name.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -14,4 +14,4 @@ echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
 
-eb deploy farmally-prod
+eb deploy farmally
