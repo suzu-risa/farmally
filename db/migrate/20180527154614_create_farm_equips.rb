67648f45b1b0b2403176ade1cf8918c74e202e03
tomoya <tom@tomhonjo.me>
2018-05-28 00:46:31 +0900

    Fix migration

--- db/migrate/20180527154614_create_farm_equips.rb
+++ db/migrate/20180527154614_create_farm_equips.rb
@@ -1,34 +1,21 @@
 class CreateFarmEquips < ActiveRecord::Migration[5.2]
   def change
     create_table :farm_equips do |t|
-      t.string :maker_price
-      t.string :integer
-      t.string :used_price
-      t.string :integer
+      t.integer :maker_price
+      t.integer :used_price
       t.string :maker
       t.string :model
       t.string :horse_power
-      t.string :string
       t.string :josu
-      t.string :integer
-      t.string :model_year
-      t.string :integer
+      t.integer :model_year
       t.string :drive_system
-      t.string :string
       t.string :safety_frame
-      t.string :string
       t.string :rotary
-      t.string :string
       t.string :hour_meter
-      t.string :string
       t.string :accessories
-      t.string :string
       t.string :location
-      t.string :string
       t.string :condition
-      t.string :string
-      t.string :category_id
-      t.string :integer
+      t.integer :category_id
 
       t.timestamps
     end
