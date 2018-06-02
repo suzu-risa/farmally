e7ab05820156e239ebd891ce387b2817e140e5d0
tmyjoe <tom@tomhonjo.me>
2018-06-02 23:24:50 +0900

    Fix deployment.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -14,4 +14,9 @@ echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
 
-eb deploy farmally
+
+if [ $1 = "prod" ]; then
+  eb deploy farmally
+else
+  eb deploy farmally-staging
+fi
