7fc5929f3c89b0c9817b6a7e60adf76f49c9ddf6
tmyjoe <tom@tomhonjo.me>
2018-06-02 23:42:08 +0900

    Fix init error.

--- app/controllers/maker_controller.rb
+++ app/controllers/maker_controller.rb
@@ -1,20 +1,23 @@
 class MakerController < ApplicationController
-  @items = Item.all
-  @maker = Maker.where(code: @params['maker_code'])
-  @title = @maker.name
-  @breadcrumb = [
-      {
-          name: 'トップ',
-          path: '/'
-      },
-      {
-          name: @maker.name,
-          path: "/makers/#{@maker.code}"
-      }
-  ]
-  @page = {
-      min: 1,
-      current: 2,
-      max: 6,
-  }
+
+  def index
+    @items = Item.all
+    @maker = Maker.where(code: @params['maker_code']).first
+    @title = @maker.name
+    @breadcrumb = [
+        {
+            name: 'トップ',
+            path: '/'
+        },
+        {
+            name: @maker.name,
+            path: "/makers/#{@maker.code}"
+        }
+    ]
+    @page = {
+        min: 1,
+        current: 2,
+        max: 6,
+    }
+  end
 end
