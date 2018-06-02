afb690de5569e446e7a0ee77ff56e98f48595f20
tmyjoe <tom@tomhonjo.me>
2018-06-02 21:42:07 +0900

    Add list page.

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,4 +1,21 @@
 class CategoryController < ApplicationController
   def index
+    @items = Item.all
+    @category = Category.where(code: 'tractor').first
+    @breadcrumb = [
+        {
+            name: 'トップ',
+            path: '/'
+        },
+        {
+            name: @category.name,
+            path: "/categories/#{@category.code}"
+        }
+    ]
+    @page = {
+        min: 1,
+        current: 2,
+        max: 6,
+    }
   end
 end
