e47049127479ad3d878cc8a61f62f700bc119c5a
Yosuke Saito <yosuke.saito1202@gmail.com>
2018-06-26 00:58:52 +0900

    add how to add flash

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -2,6 +2,10 @@ module Admin
   class HomeController < Admin::ApplicationController
     def index; end
 
-    def import; end
+    def import
+      # flash.now[:error] = ['Some errors occured', 'hogehoge'].join('<br />')
+      # render :index
+      redirect_to admin_root_url, notice: '商品を追加しました'
+    end
   end
 end
