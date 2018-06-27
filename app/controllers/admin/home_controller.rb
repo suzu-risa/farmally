91e1712180a6821f7b84a75d5038e7932dc903d9
Yosuke Saito <yosuke.saito1202@gmail.com>
2018-06-28 01:18:10 +0900

    create csv import feature

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -3,10 +3,13 @@ module Admin
     def index; end
 
     def import
-      Item.import(params[:file])
-      # flash.now[:error] = ['Some errors occured', 'hogehoge'].join('<br />')
-      # render :index
-      redirect_to admin_root_url, notice: '商品を追加しました'
+      result = Item.import(params[:file])
+      if result[:success]
+        redirect_to admin_items_url, notice: '商品を追加しました'
+        return
+      end
+      flash.now[:error] = result[:messages].join('<br />')
+      render :index
     end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,24 +12,55 @@ class Item < ApplicationRecord
   def self.import(file)
     categories_hash = Hash[*Category.all.map { |c| [c.name, c] }.flatten]
     makers_hash = Hash[*Maker.all.map { |m| [m.name, m] }.flatten]
-    ActiveRecord::Base.transaction do
-      CSV.foreach(file.path, headers: true) do |row|
-        attributes = row.to_hash.slice(*acceptable_attributes)
-        item = Item.new(
-          maker_price: attributes['maker_price'],
-          used_price: attributes['used_price'],
-          model: attributes['model'],
-          size: attributes['size'],
-          weight: attributes['weight'],
-          category: categories_hash[attributes['category']],
-          maker: makers_hash[attributes['maker']]
-        )
-        item.save!
+
+    unless utf8_encoding?(file)
+      return { success: false, messages: ['ファイルの文字コードはUTF-8である必要があります'] }
+    end
+
+    items = []
+    error_messages = []
+    CSV.foreach(file.path, headers: true) do |row|
+      attributes = row.to_hash.slice(*acceptable_attributes)
+      item = Item.new(
+        maker_price: attributes['maker_price'],
+        used_price: attributes['used_price'],
+        model: attributes['model'],
+        size: attributes['size'],
+        horse_power: attributes['horse_power'],
+        weight: attributes['weight'],
+        category: categories_hash[attributes['category']],
+        maker: makers_hash[attributes['maker']]
+      )
+      items << item
+      if item.invalid?
+        joined_message = item.errors.full_messages.join(' ')
+        error_messages << "#{$INPUT_LINE_NUMBER}行目: #{joined_message}"
       end
     end
+
+    if error_messages.present?
+      return { success: false, messages: error_messages }
+    end
+
+    ActiveRecord::Base.transaction { items.each(&:save!) }
+    { success: true }
+  rescue CSV::MalformedCSVError => _e
+    { success: false, messages: ['CSVのパース中にエラーが発生しました'] }
+  rescue StandardError => e
+    Rails.logger.error e
+    Rails.logger.error e.backtrace.join("\n")
+    { success: false, messages: ['エラーが発生しました'] }
   end
 
   def self.acceptable_attributes
     %w[maker_price used_price model horse_power size weight category maker]
   end
+
+  def self.utf8_encoding?(file)
+    s = IO.binread(file.path)
+    s.force_encoding(Encoding::UTF_8)
+    s.valid_encoding?
+  end
+
+  private_class_method :acceptable_attributes, :utf8_encoding?
 end
