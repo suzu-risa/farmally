14823e3c2de7986e274d84365f6d3efecd51982f
Yosuke Saito <yosuke.saito1202@gmail.com>
2018-06-28 23:59:16 +0900

    add static pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
   def index
     @category = Category.all
   end
+
+  def terms; end
+
+  def privacy; end
+
+  def commercial; end
+
+  def company; end
 end
