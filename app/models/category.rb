f11a998b464fad79dd3e34c5259a089514a5b417
Yosuke Saito <yosuke.saito1202@gmail.com>
2018-06-30 23:26:50 +0900

    add normalize_price method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :items
 
-  validates :name, presence: true
-  validates :code, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :code, presence: true, uniqueness: true
 end
