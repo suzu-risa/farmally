c4c4cef5c33ad176e9d26cf5525c20dbda4b60b7
Yosuke Saito <yosuke.saito1202@gmail.com>
2018-07-03 00:48:49 +0900

    refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,26 @@ class HomeController < ApplicationController
     @category = Category.all
   end
 
+  def search
+    @category = Category.find_by(code: params[:category])
+    @maker = Maker.find_by(code: params[:maker])
+    @items = Item.where(category_id: @category.id, maker_id: @maker.id).all
+    @breadcrumb = [
+      {
+        name: 'トップ',
+        path: '/'
+      },
+      {
+        name: @category.name,
+        path: "/categories/#{@category.code}"
+      },
+      {
+        name: @maker.name,
+        path: "/makers/#{@maker.code}"
+      }
+    ]
+  end
+
   def terms; end
 
   def privacy; end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :code, presence: true, uniqueness: true
+
+  def to_param
+    category_code
+  end
 end
