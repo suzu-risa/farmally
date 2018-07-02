ba9aadb491be5d7d7a1719525c37a9f83cabde06
Yosuke Saito <yosuke.saito1202@gmail.com>
2018-07-03 00:33:58 +0900

    add sell form link

---
+++ config/initializers/constants.rb
@@ -0,0 +1,4 @@
+module Constants
+  INQUIRY_URL = 'https://docs.google.com/forms/d/10ZmMloWtB2F4EVF9qq9HGerocINm9duIK7HtcIh4bWY/viewform'.freeze
+  SELL_INQUIRY_URL = 'https://docs.google.com/forms/d/10ZmMloWtB2F4EVF9qq9HGerocINm9duIK7HtcIh4bWY/viewform'.freeze
+end
--- config/initializers/inquiry_url.rb
+++
@@ -1,1 +0,0 @@
-INQUIRY_URL = 'https://docs.google.com/forms/d/10ZmMloWtB2F4EVF9qq9HGerocINm9duIK7HtcIh4bWY/viewform?edit_requested=true'.freeze
