33a1559e0feca4ec625d8a4f601a28e730f70aea
Yosuke Saito <yosuke.saito1202@gmail.com>
2018-08-15 14:05:29 +0900

    bugfix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
   // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(
+  const $navbarBurgers = Array.prototype.slice.call(
     document.querySelectorAll(".navbar-burger"),
     0
   );
@@ -27,8 +27,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     $navbarBurgers.forEach(el => {
       el.addEventListener("click", () => {
         // Get the target from the "data-target" attribute
-        var target = el.dataset.target;
-        var $target = document.getElementById(target);
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle("is-active");
