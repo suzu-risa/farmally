e6f6c8ea8431fba7c55b54d7fd64f2bd21f72507
Masato Oba <dev@masatooba.com>
2018-12-02 13:40:03 +0900

    出品商品画像用API作成 #51

--- app/controllers/sale_items_controller.rb
+++ app/controllers/sale_items_controller.rb
@@ -1,8 +1,26 @@
 class SaleItemsController < ApplicationController
   def show
-    @item = Item.find(params[:item_id])
-    @sale_item = SaleItem.find(params[:id])
+    @item = find_item(params[:item_id])
+    @sale_item = find_sale_item(params[:id])
     @title = @item.model
     @breadcrumb = breadcrumb(item: @item)
   end
+
+  def images
+    sale_item = find_sale_item(params[:sale_item_id])
+
+    render json: {
+      sale_item_images: sale_item.images.map{ |image| { url: Rails.application.routes.url_helpers.url_for(image) } }
+    }
+  end
+
+  private
+
+    def find_item(item_id)
+      Item.find(item_id)
+    end
+
+    def find_sale_item(sale_item_id)
+      SaleItem.find(sale_item_id)
+    end
 end
