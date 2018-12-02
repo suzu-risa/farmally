d0d8cb245eb97523ea5a0f89525135269f7046c2
Masato Oba <dev@masatooba.com>
2018-12-02 12:43:32 +0900

    Reactをinstallしてslickが動くところまで #56

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./components/App";
 import Search from "./components/SearchField";
 
+import React from "react";
+import ReactDOM from "react-dom";
+import Slider from "react-slick";
+
 Vue.config.productionTip = false;
 
 const isAppMountable = !!document.getElementById("app");
@@ -14,3 +18,41 @@ if (isAppMountable) {
 new Vue({
   render: h => h(Search)
 }).$mount("#search");
+
+class SimpleSlider extends React.Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<SimpleSlider />, document.getElementById("sale-item-image"));
+});
