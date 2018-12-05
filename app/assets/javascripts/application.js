e07d05a7ee86d462f548901f477c10dc5c1cfbf8
Masato Oba <dev@masatooba.com>
2018-12-05 22:05:19 +0900

    問合せモーダル用のモーダル表示まで実装 #56

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -126,4 +126,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
       btn.classList.add("hide");
     });
   });
+
+
+  document.querySelector('a#open-modal').addEventListener('click', function(event) {
+    event.preventDefault();
+    var modal = document.querySelector('.modal');  // assuming you have only 1
+    var html = document.querySelector('html');
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+
+    modal.querySelector('.modal-close').addEventListener('click', function(e) {
+      e.preventDefault();
+      modal.classList.remove('is-active');
+      html.classList.remove('is-clipped');
+    });
+  });
 });
