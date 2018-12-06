ac9f4d3904b3f024b3905c86a0450ce10a880f3c
Masato Oba <dev@masatooba.com>
2018-12-06 22:21:33 +0900

    要素の存在チェック漏れのバグ修正 #56

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -128,17 +128,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
 
-  document.querySelector('a#open-modal').addEventListener('click', function(event) {
-    event.preventDefault();
-    var modal = document.querySelector('.modal');  // assuming you have only 1
-    var html = document.querySelector('html');
-    modal.classList.add('is-active');
-    html.classList.add('is-clipped');
+  const openModalLink = document.querySelector('a#open-modal');
+  if(openModalLink) {
+    openModalLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      var modal = document.querySelector('.modal');  // assuming you have only 1
+      var html = document.querySelector('html');
+      modal.classList.add('is-active');
+      html.classList.add('is-clipped');
 
-    modal.querySelector('.modal-close').addEventListener('click', function(e) {
-      e.preventDefault();
-      modal.classList.remove('is-active');
-      html.classList.remove('is-clipped');
+      modal.querySelector('.modal-close').addEventListener('click', function(e) {
+        e.preventDefault();
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+      });
     });
-  });
+  }
 });
