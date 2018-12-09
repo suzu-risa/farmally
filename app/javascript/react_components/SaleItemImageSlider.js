9d076e06f0d9f447d67ede9e74df452fd0b7c3fa
Masato Oba <dev@masatooba.com>
2018-12-09 11:30:08 +0900

    サムネイルクリックでスライドされるように実装 #56

--- app/javascript/react_components/SaleItemImageSlider.js
+++ app/javascript/react_components/SaleItemImageSlider.js
@@ -29,26 +29,56 @@ class SaleItemImageSlider extends React.Component {
 
     const itemImages = this.state.itemImages.map((image, i)=>{
       return (
-        <img src={image.url} />
+        <img src={image.url}/>
       )
     })
 
-    const thumbnailItemImages = this.state.itemImages.map((image, i)=>{
-      return (
-        <div className="column is-mobile-6">
-          <img src={image.url} />
+    const thumbnailItemImages = this.state.itemImages.reduce((elements, image, i)=>{
+      elements = elements || [];
+
+      const lastColumns = elements[elements.length - 1];
+      var insertIndex = 0;
+
+      if(lastColumns){
+        if(lastColumns.length < 4) {
+          insertIndex = elements.length - 1;
+        } else {
+          insertIndex = elements.length;
+          elements[insertIndex] = [];
+        }
+      } else {
+        elements[0] = [];
+      }
+
+      const onClick = (e)=> {
+        return this.slider.slickGoTo(e.target.getAttribute("data-image-index"));
+      }
+
+      elements[insertIndex].push(
+        <div className="column is-3-mobile is-3-tablet is-3-desktop" data-image-index={i} onClick={ e => onClick(e)}>
+          <img src={image.url} data-image-index={i} />
         </div>
       )
-    })
+
+      return elements;
+    }, [])
+
+    const thumbnailColumns = ()=> {
+      return thumbnailItemImages.map((columns, image)=>{
+        return(
+          <div className="columns is-mobile">
+            {columns}
+          </div>
+        )
+      });
+    }
 
     return (
       <div className="container">
-        <Slider {...settings}>
+        <Slider ref={slider => (this.slider = slider)} {...settings}>
           {itemImages}
         </Slider>
-        <div className="columns is-mobile">
-          {thumbnailItemImages}
-        </div>
+        {thumbnailColumns()}
       </div>
     );
   }
