713434b5805a447d152a110c12b9cccb8ec4833d
Masato Oba <dev@masatooba.com>
2018-12-13 00:03:30 +0900

    出品商品詳細画面に追加項目表示 #51

--- app/decorators/sale_item_decorator.rb
+++ app/decorators/sale_item_decorator.rb
@@ -1,20 +1,24 @@
 module SaleItemDecorator
   def attribute_list_text
-    [year_text, prefecture_name, status_i18n, horse_power_text, used_hours_text]
+    [year_text, prefecture_name, status_i18n, horse_power_with_unit, used_hours_text]
       .select(&:present?).join(" / ")
+  end
+
+  def year_text
+    year ? "#{year}#{self.class.human_attribute_name(:year)}" : nil
+  end
+
+  def horse_power_with_unit
+    horse_power ? "#{horse_power}#{self.class.human_attribute_name(:horse_power)}" : nil
+  end
+
+  def used_hours_with_unit
+    used_hours ? "#{used_hours}h" : nil
   end
 
   private
 
-    def year_text
-      year ? "#{year}年式" : nil
-    end
-
-    def horse_power_text
-      horse_power ? "#{horse_power}馬力" : nil
-    end
-
     def used_hours_text
-      used_hours ? "アワーメーター#{used_hours}h" : nil
+      used_hours ? "#{self.class.human_attribute_name(:used_hours)}#{used_hours_with_unit}" : nil
     end
 end
