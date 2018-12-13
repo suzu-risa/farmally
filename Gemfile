affbc07e78ab0b0b247c4bf4a22f4353bbec4912
Masato Oba <dev@masatooba.com>
2018-12-13 21:26:37 +0900

    slim-railsのinstall #57

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ gem 'ckeditor'
 gem 'jp_prefecture'
 gem 'enum_help'
 gem 'active_decorator'
+gem 'slim-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Farmally
     config.autoload_paths << Rails.root.join('lib')
     config.eager_load_paths << Rails.root.join('lib')
 
+    config.generators.template_engine = :slim
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
