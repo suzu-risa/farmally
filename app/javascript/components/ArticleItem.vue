4ba5ce5ee2f5b44d4e791fa69b83a1010248ac41
Masato Oba <dev@masatooba.com>
2018-12-17 21:57:05 +0900

    最新記事のリンク修正 #57

--- app/javascript/components/ArticleItem.vue
+++ app/javascript/components/ArticleItem.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="article column is-4-tablet is-4-desktop">
-    <div class="article-column-content">
-      <div v-if="article.image" class="thumbnail" :style="{ backgroundImage: `url(${article.image})` }" />
-      <div v-else class="thumbnail" />
-      <div class="content">
-        <a :href="article.link">
+    <a :href="article.link">
+      <div class="article-column-content">
+        <div v-if="article.image" class="thumbnail" :style="{ backgroundImage: `url(${article.image})` }" />
+        <div v-else class="thumbnail" />
+        <div class="content">
           <h3 class="title">{{article.title}}</h3>
-        </a>
-        <p class="description">{{`${article.description.substr(0, 100)}...`}}</p>
+          <p class="description">{{`${article.description.substr(0, 100)}...`}}</p>
+        </div>
       </div>
-    </div>
+    </a>
   </div>
 </template>
 
