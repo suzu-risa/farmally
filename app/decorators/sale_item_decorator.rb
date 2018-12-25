f68623e3af7ce5644ef99e2878ac3baf188614ca
Masato Oba <dev@masatooba.com>
2018-12-25 23:05:06 +0900

    自動で馬力の単位の付与を無くす。入力がないときは-を表示する #116

--- app/decorators/sale_item_decorator.rb
+++ app/decorators/sale_item_decorator.rb
@@ -1,23 +1,21 @@
 module SaleItemDecorator
   def year_text
-    year ? "#{year}#{self.class.human_attribute_name(:year)}" : nil
+    year? ? "#{year}#{self.class.human_attribute_name(:year)}" : "-"
   end
 
   def horse_power_with_unit
-    horse_power ? "#{horse_power}#{self.class.human_attribute_name(:horse_power)}" : nil
+    horse_power? ? "#{horse_power}" : "-"
   end
 
   def used_hours_with_unit
-    used_hours ? "#{used_hours}h" : nil
+    used_hours? ? "#{used_hours}h" : "-"
   end
 
   def show_path
     item_sale_item_path(id, item_id: item_id)
   end
 
-  private
-
-    def used_hours_text
-      used_hours ? "#{self.class.human_attribute_name(:used_hours)}#{used_hours_with_unit}" : nil
-    end
+  def display_status
+    status? ? status_i18n : "-"
+  end
 end
