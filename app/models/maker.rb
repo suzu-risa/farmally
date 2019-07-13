18f0c9608816a05ba2abcdf8f1d5c2414e2fdc64
tmyjoe <tomoya.honjo22@gmail.com>
2019-07-13 18:14:31 +0900

    Fix.

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -18,7 +18,7 @@ class Maker < ApplicationRecord
   SellMakers = [
       {slug: "kubota", title: "クボタ", image: 'https://cosmic-s3.imgix.net/f0d7b150-696d-11e9-bd10-87624fd5798e-kubota.gif'},
       {slug: "yanmar", title: "ヤンマー", image: "https://cosmic-s3.imgix.net/f7ff1310-79f8-11e9-ace3-2b57a47304de-imgsite-logo01.png"},
-      {slug: "combine", title: "コンバイン", image: "https://cosmic-s3.imgix.net/88c2b4b0-79f9-11e9-a6b7-e5f2fd56d432-iseki.svg"}
+      {slug: "iseki", title: "イセキiseki", image: "https://cosmic-s3.imgix.net/88c2b4b0-79f9-11e9-a6b7-e5f2fd56d432-iseki.svg"}
   ]
 
   def to_param
